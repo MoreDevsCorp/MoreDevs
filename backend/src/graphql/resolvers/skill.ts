1ae80b7e9262908e3e2ffc4cf1118c715a006daf
Mustapha Limar <tapham079@gmail.com>
2023-06-22 13:45:32 +0100

    skill resolver updates

--- backend/src/graphql/resolvers/skill.ts
+++ backend/src/graphql/resolvers/skill.ts
@@ -6,8 +6,6 @@ export default {
     getSkills: async (_: any, __: any, context: Context) => {
       const { session, prisma } = context;
 
-      console.log("Reached here");
-
       if (!session?.user) {
         throw new GraphQLError("You're not authenticated !", {
           extensions: { code: 401 },
