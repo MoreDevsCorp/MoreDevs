88afe11b5924174fe84e50700aac7ef0435491e6
mustaphalimar <tapham079@gmail.com>
2023-09-13 19:51:06 +0100

    added env var

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -146,8 +146,8 @@ async function main() {
   app.use(
     "/graphql",
     cors<cors.CorsRequest>({
-      // origin: [process.env.CLIENT_ORIGIN as string],
-      origin: "*",
+      origin: [process.env.CLIENT_ORIGIN as string],
+      // origin: "*",
       credentials: true,
     }),
     json(),
